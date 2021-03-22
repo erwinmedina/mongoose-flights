71cfe72a66341827ae147d478184afd30712022a
Erwin Medina <erwin.l.medina@gmail.com>
2021-03-22 11:44:43 -0700

    added beginning of lab part 2

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema; 
+
+const destinationSchema = new Schema (
+    {
+        airport: {
+            type: String,
+            enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        },
+        arrival: Date,
+    
+    }, {
+        timestamps: true
+    }
+)
+
 const flightSchema = new Schema (
     {
         property: String,
@@ -12,6 +26,10 @@ const flightSchema = new Schema (
                 const current = new Date();
                 const date = current.setFullYear(current.getFullYear() + 1);
                 return date;
-             }}
-        });
+             }},
+        destinations: [destinationSchema]
+        }
+        
+        );
+
 module.exports = mongoose.model('Flight', flightSchema);
